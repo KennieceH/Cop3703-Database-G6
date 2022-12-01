9ba0518bd292d83a61d149eb7be98457838467ee
KennieceH <kenniece.m.harris@gmail.com>
2022-11-30 19:02:11 -0500

    Query 4 completed
    Uncompleted Queries 3,5,6

--- Database_G6/src/g6/Insert.java
+++ Database_G6/src/g6/Insert.java
@@ -5,7 +5,9 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import java.util.Scanner;
@@ -17,18 +19,24 @@ class Insert {
 	{
 		
 	 Scanner input = new Scanner(System.in);
-	  System.out.println("Hello, what data would you like to insert?");
+	  System.out.println("Hello, what data would you like to insert or search?");
 	  System.out.println("1. Department");
 	  System.out.println("2. Student");
 	  System.out.println("3. Instructor");
+	  System.out.println("4. Enroll a Student into a Course/Section");
+	  System.out.println("5. Find Courses offered by Department Name/Code");
+	  System.out.println("6. List all Course/Sections for an Instructor");
 	 
 	  int userInput = input.nextInt();
 	  
 	  switch(userInput) {
 	  
 	  case 1: InsertDept(); break;
-	  case 2: break;
-	  case 3: break;
+	  case 2: InsertStudent(); break;
+	  case 3: InsertInstructor();break;
+	  case 4: InsertEnrollStud(); break;
+	  case 5: SelectCourseDept(); break;
+	  case 6: SelectCourseInst(); break;
 	  }
 	  
 	
@@ -241,6 +249,166 @@ class Insert {
 	      done = getInt();
 	      } // while done
 	    conn.close();
+    }
+    
+    public static void InsertEnrollStud() throws SQLException {
+    	String uid = "G6";
+	     
+
+	     
+	      String pword = "Fall2022G6";
+	      
+
+	    String url = "jdbc:oracle:thin:@cisvm-oracle.unfcsd.unf.edu:1521:orcl";
+	    // Load the Oracle JDBC driver
+	    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+	    // Connect to the database
+	    Connection conn =
+	      DriverManager.getConnection (url, uid, pword);
+
+	    // Prepare to insert new vendors into the VENDOR table
+	    PreparedStatement pstmt =
+	    	      conn.prepareStatement ("INSERT INTO ENROLLS_IN(stud_n_num, section_num, sec_year, semester, course_num, grade) " +
+	    	      "VALUES (?, ?, ?, ?, ?, ?)");
+
+	    int done = 1;
+
+	    while (done != 0) {
+	      System.out.println("\n Enter student N number: ");
+	      String snnum = getString();
+	      System.out.println("\nEnter section number: ");
+	      int secnum = getInt();
+	      System.out.println("\n Enter section year: ");
+	      String secyear = getString();
+	      System.out.println("\n Enter semester: ");
+	      String semester = getString();
+	      System.out.println("\n Enter course number: ");
+	      String cnum = getString();
+	      System.out.println("\n Enter grade: ");
+	      String grade = getString();
+	     
+
+
+	      pstmt.setString(1, snnum);
+	      pstmt.setInt(2, secnum);
+	      pstmt.setString(3, secyear);
+	      pstmt.setString(4, semester);
+	      pstmt.setString(5, cnum);
+	      pstmt.setString(6, grade);
+	        
+	      int NumRows = pstmt.executeUpdate();
+	      System.out.println("\n" + NumRows + " row(s) inserted");
+
+	      System.out.println("\nHit 0 for exit, " +
+		     "or enter any other number for another insert: ");
+	      done = getInt();
+	      } // while done
+	    conn.close();
+    }
+    
+    public static void SelectCourseDept() throws SQLException {
+    	String uid = "G6";
+	     
+
+	     
+	     String pword = "Fall2022G6";
+	      
+
+	    String url = "jdbc:oracle:thin:@cisvm-oracle.unfcsd.unf.edu:1521:orcl";
+	    // Load the Oracle JDBC driver
+	    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+	    // Connect to the database
+	    Connection conn =
+	      DriverManager.getConnection (url, uid, pword);
+	    Statement stmt = conn.createStatement ();
+
+	    System.out.print("\n Would you like to search by department code or name?: ");
+	    String dept = getString();
+        String v = "";
+	      // Select the V_NAME column from the VENDOR table
+          String q = "";
+	     if(dept.equals("code")) {
+	    	 System.out.print("\n Please enter department code: ");
+	    	 v = getString();
+	    	 q = "SELECT COURSE.course_name, COURSE.course_num " +
+		         "FROM DEPARTMENT, COURSE " +
+		         "WHERE DEPARTMENT.dept_code= " + v + " AND DEPARTMENT.dept_name = COURSE.dept_name";
+	     }
+	     
+	     else {
+	    	 System.out.print("\n Please enter department name: ");
+	    	 v = getString();
+	    	 q = "SELECT COURSE.course_name, COURSE.course_num " +
+			         "FROM DEPARTMENT, COURSE " +
+			         "WHERE DEPARTMENT.dept_name=" + "'" + v + "'"+ " AND DEPARTMENT.dept_name = COURSE.dept_name";
+	     }
+	    	 
+	       
+		  
+	    
+	     ResultSet rset = stmt.executeQuery(q);
+
+	     while(rset.next ()) {
+	       String cname = rset.getString("course_name");
+	       String cnum = rset.getString("course_num");
+	            
+	       System.out.println("Course Number:    " + "Course Name: ");
+	       System.out.printf("%s %20s", cnum, cname);
+	       System.out.println("");
+	       System.out.println("");
+	     } //if
+
+	     stmt.close();
+         conn.close();
+    }
+    
+    public static void SelectCourseInst() throws SQLException {
+    	String uid = "G6";
+	     
+
+	     
+	      String pword = "Fall2022G6";
+	      
+
+	    String url = "jdbc:oracle:thin:@cisvm-oracle.unfcsd.unf.edu:1521:orcl";
+	    // Load the Oracle JDBC driver
+	    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+	    // Connect to the database
+	    Connection conn =
+	      DriverManager.getConnection (url, uid, pword);
+	    Statement stmt = conn.createStatement ();
+
+	    System.out.print("\nEnter existing vendor code: ");
+	    int v = getInt();
+
+	      // Select the V_NAME column from the VENDOR table
+
+	      String q = "SELECT * " +
+	               "FROM VENDOR " +
+	               "WHERE V_CODE= " + v ;
+		  
+	     
+	     ResultSet rset = stmt.executeQuery(q);
+
+	     if (rset.next ()) {
+	       String vname = rset.getString("V_NAME");
+	       String vcontact = rset.getString("V_CONTACT");
+	       int vareacode = rset.getInt("V_AREACODE");
+	       String vphone = rset.getString("V_PHONE");       
+	       String vstate = rset.getString("V_STATE");       
+	       
+
+	       System.out.println("\nVendor name is: " + vname);
+	       System.out.println("Vendor contact is: " + vcontact);
+	       System.out.println("Vendor phone is: (" + vareacode + ")" + vphone);
+	       System.out.println("Vendor state is: " + vstate);
+	     } //if
+
+	     stmt.close();
+         conn.close();
     }
        
 	public static String getString() {
